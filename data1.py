a3ce198131b8ca66814554d2f370435e28e5f793
thecoderarnav <60697179+thecoderarnav@users.noreply.github.com>
2021-02-16 16:31:14 +0530

    Add files via upload

---
+++ data1.py
@@ -0,0 +1,6 @@
+import csv
+import plotly.express as  px 
+with open ("data.csv")  as csv_file:
+    df = csv.DictReader(csv_file)
+    fig = px.scatter(df, x = "Temperature", y = "Ice-cream Sales")
+    fig.show()
